6d53f83e0ee133b97ad597e661c627fb3a3a6dfe
abhi0002 <abhipsa.hota002@gmail.com>
2019-03-05 18:11:46 +0530

    Final commit of the day

--- Node/hello-world/node-app/app.js
+++ Node/hello-world/node-app/app.js
@@ -1,8 +1,10 @@
 console.log('Hello World');
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'Hello i am notes for the second hit. And this time the previous text will be overwitten with this new one.');
+fs.writeFileSync('notes.txt',
+ 'Hello i am notes for the second hit.And this time the previous text will be overwitten with this new one.');
 
 // Challange 1
     // Append a message to notes.txt
-fs.appendFileSync('notes.txt' , 'Now this one will be appended, not overwritten')
+fs.appendFileSync('notes.txt' ,
+ 'Now this one will be appended, not overwritten')
